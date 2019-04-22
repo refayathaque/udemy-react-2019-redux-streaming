31d2453eaa6c99ca566a368014b2d09cd1e909b3
Refayat Haque <refayathaque@gmail.com>
2019-04-22 16:58:27 -0400

    staged, committed and pushing to master

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
 
 const PageOne = () => {
   return (
     <div>
       PageOne
+      <Link to="/pagetwo">Navigate to Page Two</Link>
     </div>
   )
 }
@@ -13,6 +14,7 @@ const PageTwo = () => {
   return (
     <div>
       PageTwo
+      <Link to="/">Navigate to Page One</Link>
     </div>
   )
 }
@@ -22,7 +24,7 @@ const App = () => {
     <div>
       <BrowserRouter>
         <div>
-          <Route path="/" exact component={PageOne} />
+          <Route path="/" exact={true} component={PageOne} />
           <Route path="/pagetwo" component={PageTwo} />
         </div>
       </BrowserRouter>
