f3b2a1623e310158a603403e55b498cd1c2eecc0
Refayat Haque <refayathaque@gmail.com>
2019-04-22 15:49:46 -0400

    staged, committed and pushing to master

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,31 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+const PageOne = () => {
+  return (
+    <div>
+      PageOne
+    </div>
+  )
+}
+
+const PageTwo = () => {
+  return (
+    <div>
+      PageTwo
+    </div>
+  )
+}
 
 const App = () => {
   return (
     <div>
-      App
+      <BrowserRouter>
+        <div>
+          <Route path="/" exact component={PageOne} />
+          <Route path="/pagetwo" component={PageTwo} />
+        </div>
+      </BrowserRouter>
     </div>
   );
 };
