7a0edfb25babfe57d3cc322b0588b90c5de76391
Refayat Haque <refayathaque@gmail.com>
2019-04-30 15:59:34 -0400

    staged, committed and pushing to master

--- src/components/dumb/App.js
+++ src/components/dumb/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
+
 // BrowserRouter is the MOST COMPLICATED TO DEPLOY in most settings (there are exceptions to this, SOME deployment services will make deployments with BrowserRouter easy), and this is because most servers will not automatically serve up the `index.html` file in the public folder once it realizes that it does not have any data on requested routes other than "/" (only React servers will automatically serve up `index.html`)
 // Default behavior for most servers when it does not have data on routes other than "/" is responding with a 404
 // HashRouter solves the issue above, because it will make sure that for every request made to the server the server responds with the "/" route (our index.html file) irrespective of what follows the "#", the backend server will not look at anything after the "#", and only the client/browser will look at what is after the "#"
@@ -9,6 +10,7 @@ import { BrowserRouter, Route } from 'react-router-dom';
 // "If you are using a dynamic server that can handle dynamic URLs then you need to use the BrowserRouter component but if you are using a server that only serves static files then a HashRouter component is what to be used in this case." - https://www.techiediaries.com/react-router-dom-v4/
 // MemoryRouter - "A <Router> that keeps the history of your "URL" in memory (does not read or write to the address bar). Useful in tests and non-browser environments like React Native." - https://github.com/ReactTraining/react-router/blob/master/packages/react-router/docs/api/MemoryRouter.md
 // `MemoryRouter` and `HashRouter` must be included in the list of named imports from 'react-router-dom' above
+
 import Header from 'components/dumb/Header';
 import StreamList from 'components/streams/StreamList';
 import StreamCreate from 'components/streams/smart/StreamCreate';
--- src/components/streams/smart/StreamCreate.js
+++ src/components/streams/smart/StreamCreate.js
@@ -2,24 +2,53 @@ import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
 // `Field` is just a component that hooks up all the redux-form infrastructure, the infrastructure that handles the action creators/reducers for our forms and persists the form data in the Redux store
 
+// Redux-form is a way of maintaining 'controlled' elements in forms, but instead of using localized class-based state to maintain the form data, we are using the Redux store to establish a global application-level state to maintain the form data
+
 class StreamCreate extends Component {
-  renderInput({ input }) {
-    // ^ ES6 destructuring of `formsProps.input`
+  renderInput({ input, label }) {
+    // ^ ES6 destructuring of `formsProps.input` and `formsProps.label`
     // console.log(input)
     return (
-      <input { ...input } />
-      // `formProps.input` has the properties `value` and the `onChange` handler, the spread operator above will include these ^ properties (and more..) to the input element
+      <div className="field">
+        <label>{label}</label>
+        <input { ...input } />
+        {/* `formProps.input` has the properties `value` and the `onChange` handler, the spread operator above will include these ^ properties (and more..) to the input element */}
+      </div>
     )
+  }
+
+  onSubmit(formValues) {
+    // event.preventDefault();
+    // ^ To prevent page refresh
+    validate(formValues)
   }
 
   render () {
     return (
-      <form>
-        <Field name="title" component={this.renderInput} />
-        <Field name="description" component={this.renderInput} />
+      <form className="ui form" onSubmit={this.props.handleSubmit(this.onSubmit)}>
+        {/* `handleSubmit` is how redux-form submits forms, the function will take care of things like running `event.preventDefault` to prevent page refreshes upon form submission, and it also takes in whatever we pass into it such as our `onSubmit` function defined above */}
+        {/* When it runs `onSubmit` it will pass in the `formValues` object to the function so that we can access the form values for purposes of validation or whatever else we want to do */}
+        <Field name="title" component={this.renderInput} label="Enter Title" />
+        {/* `renderInput` will get called with `formProps` from redux-form passed in */}
+        <Field name="description" component={this.renderInput} label="Enter Description" />
+        <button className="ui button primary">Submit</button>
       </form>
     );
   };
+};
+
+const validate = (formValues) => {
+  console.log(formValues)
+  const errors = {};
+  if (!formValues.title) {
+    errors.title = 'You must enter a title';
+  };
+  if (!formValues.description) {
+    errors.description = 'You must enter a description'
+  };
+  // If the form data does not pass our validation checks we return an object with key-value pairs that will have the name of the field and the error message we wish to display to the user, once we return this error details object redux-form will rerender our component
+  // We return an EMPTY object if the form data passes our validation checks, returning an EMPTY object makes redux-form think that the form is valid
+  return errors;
 };
 
 export default reduxForm({
